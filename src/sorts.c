1a04c4196268ed9ff8d106eeaf2484ccc77da24e
Julien Houyet <julien.houyet@teradev.be>
2023-12-22 18:54:48 +0100

    Add ft_sort_three

--- src/sorts.c
+++ src/sorts.c
@@ -6,7 +6,7 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/22 16:08:26 by jhouyet           #+#    #+#             */
-/*   Updated: 2023/12/22 18:09:04 by jhouyet          ###   ########.fr       */
+/*   Updated: 2023/12/22 18:53:40 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,25 @@
 
 void	ft_sort_three(t_stacks *stacks)
 {
-	ft_printf("Three sort\n");
+	int	*nb;
+
+	nb = stacks->a.nb;
+	if (nb[0] > nb[1] && nb[1] < nb[2] && nb[0] < nb[2])
+		ft_sa(stacks);
+	else if (nb[0] > nb[1] && nb[1] < nb[2] && nb[0] > nb[2])
+		ft_ra(stacks);
+	else if (nb[0] < nb[1] && nb[1] > nb[2] && nb[0] > nb[2])
+		ft_rra(stacks);
+	else if (nb[0] > nb[1] && nb[1] > nb[2])
+	{
+		ft_sa(stacks);
+		ft_rra(stacks);
+	}
+	else if (nb[0] < nb[1] && nb[1] > nb[2] && nb[0] < nb[2])
+	{
+		ft_sa(stacks);
+		ft_ra(stacks);
+	}
 }
 
 void	ft_sort_four(t_stacks *stacks)
